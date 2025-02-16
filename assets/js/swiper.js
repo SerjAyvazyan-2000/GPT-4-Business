deccf05b73a5cba62d4a705a51eb02c4742f1e3a
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-02-16 17:30:48 +0400

    first

---
+++ assets/js/swiper.js
@@ -0,0 +1,57 @@
+
+
+
+function initSwiper() {
+    let reviewsSwiper;
+
+    const isMobile = window.innerWidth <= 992;
+    const direction = isMobile ? "horizontal" : "vertical";
+
+    if (reviewsSwiper) {
+        reviewsSwiper.destroy(true, true);
+    }
+
+    reviewsSwiper = new Swiper(".reviews-swiper", {
+        direction: direction,
+        spaceBetween: 10,
+        slidesPerView: 3,
+        pagination: {
+            el: ".reviews-pagination",
+            clickable: true,
+        },
+        loop: true,
+        autoplay: {
+            delay: 10,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+
+        breakpoints: {
+            320: {slidesPerView: 1,},
+            490: {slidesPerView: 1.2,},
+            620: {slidesPerView: 1.5,},
+            810: {slidesPerView: 2},
+            992: {slidesPerView: 2},
+            1096: {slidesPerView: 2.5},
+            1232: {slidesPerView: 3},
+            1920: {slidesPerView: 3},
+        },
+
+    });
+    const swiperContainer = document.querySelector(".reviews-swiper");
+
+    if(swiperContainer){
+
+        swiperContainer.addEventListener("mouseenter", () => {
+            reviewsSwiper.autoplay.stop();
+        });
+
+        swiperContainer.addEventListener("mouseleave", () => {
+            reviewsSwiper.autoplay.start();
+        });
+    }
+
+}
+
+initSwiper();
+
